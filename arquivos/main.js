ed970031028df8cfc8625c6d8a348a7bb1c66039
NicollasCosmo <111130572+NicollasCosmo@users.noreply.github.com>
2022-10-03 10:48:23 -0300

    atualização - main,js

---
+++ arquivos/main.js
@@ -0,0 +1,29 @@
+function validarDados() {
+    var nome = document.getElementById("nomeSobrenome").value;
+    var email = document.getElementById("email").value;
+    var telefone = document.getElementById("telefone").value;
+    var mensagem = document.getElementById("mensagem").value;
+
+
+    if (nome.length>3 && email.length>3 && telefone.length>3 && mensagem.length>3)  {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+function validarTudo() {
+
+    if(validarDados()==true){
+    alert("Dados enviados com Sucesso!");
+        return true;
+
+    }
+    else {
+        alert("ERRO: Verifique se os campos Nome, Email, Telefone e Mensagem estão preenchidos corretamente.");
+        return false;
+
+    }
+}
